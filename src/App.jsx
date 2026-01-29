c33b338c86ddc5e80b00d3b9f28a12a8a5c31884
didier-bazayesu <didierbazayesu@gmail.com>
2026-01-29 12:34:46 +0200

    feat: fetching while clicking button

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,16 @@
-import axios from "axios";
-import { useEffect, useState,useContext } from "react";
-import HandlePrps from "./HandlePrps";
-import { UserDetails } from "./HandlePrps";
+import SampleFetching from "./SampleFetching";
+
 
 function App() {
 
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
+ 
 
 
-  useEffect(() => {
-    // Start timeout
-    const timeout = setTimeout(() => {
-      // If API hasn't responded in 3 seconds, show error
-      if (isLoading) {
-        setHasError(true);
-        setIsLoading(false);
-      }
-    }, 3000);
-
-    async function fetchUsers() {
-      try {
-        const response = await axios.get("https://dummyjson.com/users");
-        // API responded — cancel timeout
-        clearTimeout(timeout);
-        setData(response.data.users);
-        setIsLoading(false);
-      } catch (error) {
-        clearTimeout(timeout);
-        setHasError(true);
-        setIsLoading(false);
-        console.error(error);
-      }
-    }
-
-    fetchUsers();
-
-    // Cleanup in case component unmounts
-    return () => clearTimeout(timeout);
-  }, []);
-
-
-  function handleClick() {
-    alert("user clicked")
-  }
-
+  
   return (
-    <div>
-      <h1>Data for sample test</h1>
-
-      {/* {isLoading && <p>Page Loading...</p>}
-      {hasError && <p style={{ color: "red" }}>Failed to load data</p>}
-
-      {!isLoading && !hasError &&
-        data.map((user) => (
-          <div key={user.id} className="design-dev">
-            <p>Name: {user.firstName} {user.lastName}</p>
-            <p>Email: {user.email}</p>
-          </div>
-        ))
-      } */}
-      
-      <UserDetails.Provider value={{ onClick: handleClick }}>
-         <HandlePrps  />
-
-      </UserDetails.Provider>
-      
-    </div>
+    <>
+       <SampleFetching/>
+    </>
   );
 }
 
