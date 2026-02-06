1f2310eb7b4f89a1fdd87e2b25eb0f5af6a8ebbf
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-06 17:31:11 +0200

    feat:some update under list

--- src/List.jsx
+++ src/List.jsx
@@ -1,11 +1,16 @@
+import { func } from 'prop-types'
 import React from 'react'
+import "./name.css"
 
 function List({food}) {
+  function handleOver(){
+    return <h1 className='handle-name'>This page is created by didier</h1>
+  }
 
   return (<>
   <div>
     <h2>Fruits items</h2>
-    <ul>
+    <ul onMouseOver={handleOver}>
     {food.fruits.map(elem=> <li key={Math.random()*5}>{elem}</li>)}
     </ul>
   </div>
