a415149c5f24d387560afea55c7e9f6af3307ef7
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-06 17:49:25 +0200

    feat: done fetching and get data

--- src/Searching.jsx
+++ src/Searching.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { func } from 'prop-types';
+import HandlePrps from './HandlePrps';
 
 function Searching() {
+let[dataFetch,setData] = useState([]);
+async function handleFetching (){
+    const data = await axios.get("https://jsonplaceholder.typicode.com/users");
+    setData(()=> data.data);
+
+};
+
+
+
+useEffect(()=>{
+   handleFetching();
+},[])
   return (
     <div>
        <input placeholder='start searching....' />
