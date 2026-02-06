04e5c594f2c18e63e9a94d41e5e14dcd3b94a2e8
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-06 18:00:51 +0200

    feat: other code of search

--- src/Searching.jsx
+++ src/Searching.jsx
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { func } from 'prop-types';
-import HandlePrps from './HandlePrps';
+
 
 function Searching() {
 let[dataFetch,setData] = useState([]);
+let[change,setchange] = useState("")
 async function handleFetching (){
     const data = await axios.get("https://jsonplaceholder.typicode.com/users");
     setData(()=> data.data);
 
 };
 
-
-
-useEffect(()=>{
-   handleFetching();
+  function  handleSuch(eve){
+       setchange(eve.target.value);
+      
+  }
+  console.log(change)
+  
+  
+  
+  useEffect(()=>{
+      handleFetching();
+      handleSuch();
 },[])
   return (
     <div>
-       <input placeholder='start searching....' />
+       <input placeholder='start searching....' onChange={(e)=>handleSuch(e)}/>
     </div>
   )
 }
