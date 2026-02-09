0f7a786da26ad682602634df5a12fd3a5f5a1b6f
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-09 10:17:55 +0200

    feat: event handler change

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,12 +4,16 @@ import "./name.css"
 
 function Dashboard() {
   let[count,setCount] = useState(0)
+  let[change,setChange] = useState("")
   return( 
     <div>
+      <p>{change}</p>
+      <input type="text" onChange={(e)=> setChange(e.target.value) } />
       <div>{count}</div>
       <button onClick={() => { setCount(count + 1) }}>Add</button>
-        <button>Decrement</button>
-          <button>Reset</button>
+      <button onClick={() => { setCount(count-1) }}>Decrement</button>
+      <button onClick={() => { setCount(0) }}>Reset</button>
+
 
     </div>
   )
