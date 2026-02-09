3e6c241bbe5b69d5b106db7b9ea231bafcf5df18
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-09 10:29:21 +0200

    feat:payment

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
 import "./name.css"
 
 function Dashboard() {
   let[count,setCount] = useState(0)
   let[change,setChange] = useState("")
+  let[select,setSelect] = useState("")
   return( 
     <div>
-      <p>{change}</p>
+      <p><b>Product: </b>{change}</p>
       <input type="text" onChange={(e)=> setChange(e.target.value) } />
       <div>{count}</div>
       <button onClick={() => { setCount(count + 1) }}>Add</button>
       <button onClick={() => { setCount(count-1) }}>Decrement</button>
-      <button onClick={() => { setCount(0) }}>Reset</button>
+      <button onClick={() => { setCount(0) }}>Reset</button> <br /> <br />
+
+
+      {/* //event handler selection here and they way it performs  */}
+      <p>{ select}</p>
+
+      Payment :<select onChange={(e)=> setSelect(e.target.value) }>
+        <option value="">select the option</option>
+        <option value="Visa">Visa</option>
+        <option value="Mtn">MTN</option>
+        <option value="cash">Cash</option>
+
+      </select>
 
 
     </div>
