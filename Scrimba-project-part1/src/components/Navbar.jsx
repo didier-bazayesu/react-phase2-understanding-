fc76469516d9f2c1d6d5555e026bae731841f9d7
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-10 13:25:07 +0200

    style : main component

--- Scrimba-project-part1/src/components/Navbar.jsx
+++ Scrimba-project-part1/src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import image from '../assets/react.svg';
 import React from 'react'
 
+
 export default function 
 () {
   return (
-    <div className='handle-react-logo'>
-      <div className='handle-padding'> 
-          <img src={image} alt="" />
-          <h2>ReactFacts</h2>
-          
-      </div>
+    <header>
+      <nav>
+        <img src={image}alt="react-image" />
+        <span>ReactFacts</span>
+      </nav>
 
-    </div>
+    </header>
   )
 }
