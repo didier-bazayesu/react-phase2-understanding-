2397803fe9d99025d380a7a11e5f63734b069a78
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-10 12:09:13 +0200

    feat: render project

--- Scrimba-project-part1/src/App.jsx
+++ Scrimba-project-part1/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
   return (
+    <div className='background-App'>
     <Navbar/>
+    <Main/>
+    </div>
   )
 }
 
