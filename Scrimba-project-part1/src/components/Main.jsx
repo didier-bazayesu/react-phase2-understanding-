b1d4edad16b186b16afaf543ce7edd2f13f58193
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-10 12:10:20 +0200

    feat:Main component

---
+++ Scrimba-project-part1/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Main() {
+  return (
+      <div className='background-dev'>
+        <h1>Fun facts about React</h1>
+          <ul className='handle-ul-listItem'>
+            <li>Was first released in 2013</li>
+            <li>Was orginally created  </li>
+            <li>It mentained by Meta</li>
+            <li>Poowers thousands of enterprise apps ,mincluding mobile apps</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Main
