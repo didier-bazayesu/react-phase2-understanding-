61d2fa1550b5b2e23c0d5f6092bd47bc1006ea81
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-10 11:34:04 +0200

    chore: create first project for scrimba learning

---
+++ Scrimba-project-part1/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import image from '../assets/react.svg';
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <img src={image} alt="" />
+        
+    </div>
+  )
+}
