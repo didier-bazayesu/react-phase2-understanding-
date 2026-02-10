59f0927726349a50ee1d79ffc2f8b195b11e8f86
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-10 12:10:52 +0200

    feat:navigation bar

--- Scrimba-project-part1/src/components/Navbar.jsx
+++ Scrimba-project-part1/src/components/Navbar.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 export default function 
 () {
   return (
-    <div>
-        <img src={image} alt="" />
-        
+    <div className='handle-react-logo'>
+      <div className='handle-padding'> 
+          <img src={image} alt="" />
+          <h2>ReactFacts</h2>
+          
+      </div>
+
     </div>
   )
 }
