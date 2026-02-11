f1f0d40159a0e044e3bb6e31edf5bd8d7459fb0c
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-11 17:24:15 +0200

    feat: this is the file contains information about travel jaurnel information

---
+++ travel jaurnel/src/travleInform/travelInformation.js
@@ -0,0 +1,46 @@
+import { travelImage1, travelImage2, travelImage3, travelImage4, travelImage5 } from "../../public/image";
+
+const   entryData = [
+    {
+        image: travelImage1,
+        googleMap: "https://www.google.com/maps/place/Mount+Fuji",
+        title: "Mount Fuji",
+        countryName: "Japan",
+        date: "12 Jan, 2021 - 24 Jan, 2021",
+        paragraph: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). It is the most popular tourist site in Japan for both local and international visitors."
+    },
+    {
+        image: travelImage3,
+        googleMap: "https://www.google.com/maps/place/Eiffel+Tower",
+        title: "Eiffel Tower",
+        countryName: "France",
+        date: "5 Mar, 2021 - 12 Mar, 2021",
+        paragraph: "The Eiffel Tower is one of the most recognizable landmarks in the world. Located in Paris, it attracts millions of visitors every year."
+    },
+    {
+        image: travelImage4,
+        googleMap: "https://www.google.com/maps/place/Grand+Canyon",
+        title: "Grand Canyon",
+        countryName: "USA",
+        date: "18 Apr, 2021 - 25 Apr, 2021",
+        paragraph: "The Grand Canyon in Arizona is famous for its layered red rock formations and breathtaking views carved by the Colorado River."
+    },
+    {
+        image: travelImage5,
+        googleMap: "https://www.google.com/maps/place/Great+Wall+of+China",
+        title: "Great Wall",
+        countryName: "China",
+        date: "10 May, 2021 - 18 May, 2021",
+        paragraph: "The Great Wall of China stretches over 13,000 miles and was built to protect ancient Chinese states from invasions."
+    },
+    {
+        image: travelImage1,
+        googleMap: "https://www.google.com/maps/place/Pyramids+of+Giza",
+        title: "Pyramids of Giza",
+        countryName: "Egypt",
+        date: "2 Jun, 2021 - 9 Jun, 2021",
+        paragraph: "The Pyramids of Giza are ancient monumental tombs and one of the Seven Wonders of the Ancient World, located near Cairo."
+    }
+];
+
+export default entryData;
