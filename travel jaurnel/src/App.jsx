9b527748559f90845906d28fb2a044699b95a66d
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-11 11:52:34 +0200

    feat: rendering Headertravel.jsx component

---
+++ travel jaurnel/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderJaurnel from './components/HeaderJaurnel'
+
+function App() {
+  return (
+     <>
+     <HeaderJaurnel/>
+      
+     
+     </>
+  )
+}
+
+export default App
