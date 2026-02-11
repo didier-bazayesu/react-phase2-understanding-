ac022e5a8bc6c2585922d83528fd3dd63d057733
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-11 17:26:56 +0200

    feat: it's exporting all images to make them reusable else where

--- travel jaurnel/public/image.js
+++ travel jaurnel/public/image.js
@@ -1,4 +1,7 @@
 
 export const img = '/globe-solid-full.svg';
-export const travelImage1 = "/travel_image1.jpg"
-export const travelImage2 = "/navigation_arrow_red.png"
+export const travelImage1 = "/images/travel_image1.jpg"
+export const travelImage2 = "/images/navigation_arrow_red.png"
+export const travelImage3 = "/images/travelImage3.jpeg"
+export const travelImage4 = "/images/travelImage4.jpeg"
+export const travelImage5 = "/images/travelImage5.jpeg"
