d79011df463edd0992e6627d6009e06b3bdff5aa
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-12 21:21:56 +0200

    feat: rendering the project  of incredient

---
+++ incredientProject/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import HeaderIncredient from './components/HeaderIncredient'
+
+
+function App() {
+
+
+
+  return (
+    <div>
+      <HeaderIncredient/>
+    </div>
+  )
+}
+
+export default App
