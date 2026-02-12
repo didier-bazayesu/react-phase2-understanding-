21ab70a702e5c9ce9447263dca52618ac411b3a7
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-12 23:45:23 +0200

    feat: rendering ChangeBulb.jsx

--- incredientProject/src/App.jsx
+++ incredientProject/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import HeaderIncredient from './components/HeaderIncredient'
+import ChangeBulb from './components/changeBulb'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div>
+       <ChangeBulb/>
       <HeaderIncredient/>
     </div>
   )
